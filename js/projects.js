cab492bbe79036fb080325042865ca35569ec4fb
Abhinav Verma <abhinavverma658@gmail.com>
2023-08-25 00:35:04 +0530

    new commit

---
+++ js/projects.js
@@ -0,0 +1,9 @@
+function togglePopup_1(){
+  document.getElementById("popup-1").classList.toggle("active")
+}
+function togglePopup_2(){
+    document.getElementById("popup-2").classList.toggle("active")
+}
+function togglePopup_3(){
+    document.getElementById("popup-3").classList.toggle("active")
+}
