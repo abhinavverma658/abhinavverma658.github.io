690e9b3b6371c382217eea6839d86e15d8cdd93b
Abhinav Verma <abhinavverma658@gmail.com>
2024-04-14 14:58:47 +0530

    Latest commit

--- js/projects.js
+++
@@ -1,9 +0,0 @@
-function togglePopup_1(){
-  document.getElementById("popup-1").classList.toggle("active")
-}
-function togglePopup_2(){
-    document.getElementById("popup-2").classList.toggle("active")
-}
-function togglePopup_3(){
-    document.getElementById("popup-3").classList.toggle("active")
-}
