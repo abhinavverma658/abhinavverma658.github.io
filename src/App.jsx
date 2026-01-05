49c5f4c5bc4520eb3d3d78bce1c26caa6dad1e0a
Abhinav Verma <abhinavverma658@gmail.com>
2026-01-05 23:40:50 +0530

    creating stats section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/navbar.jsx";
 import HeroSection from "./components/herosection.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
---
+++ src/components/stats.jsx
@@ -0,0 +1,8 @@
+const statsNumber = () => {
+  return (
+    <div className="stats-number">
+      <h3 className="text-3xl font-bold text-white">150+</h3>
+      <p className="text-gray-400">Projects Completed</p>
+    </div>
+  );
+};
