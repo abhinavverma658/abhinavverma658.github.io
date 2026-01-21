3a5b667dcb167f9723997fec19993bcbb54f3965
Abhinav Verma <abhinavverma658@gmail.com>
2026-01-21 23:50:54 +0530

    adding contact card

---
+++ src/components/contact.jsx
@@ -0,0 +1,7 @@
+const Contact = () => {
+  return (
+    <div>
+      <div className="max-w-5xl mx-auto p-4 mt-8 mb-8" id="contact"></div>
+    </div>
+  );
+};
--- src/components/leaveMessage.jsx
+++ src/components/leaveMessage.jsx
@@ -1,4 +1,28 @@
+import React, { useState } from "react";
 const Message = () => {
+  const [result, setResult] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "8601b9e3-5ebc-45f0-9d01-584e4cb83236");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className="border-b border-gray-500">
       <div className="max-w-5xl mx-auto p-4 mt-8 mb-8" id="contact">
@@ -6,19 +30,21 @@ const Message = () => {
           Leave a Message
         </h2>
         <div className=" p-6 pt-8 rounded-lg shadow-md">
-          <form action="">
+          <form onSubmit={onSubmit}>
             <div className="flex justify-center gap-4">
               <div className="w-1/2 flex flex-col  ">
                 <label className="text-white pb-3">Full Name</label>
                 <input
                   type="text"
                   placeholder="Enter Name"
+                  name="name"
                   className="rounded-2xl pb-3 mb-3 text-white border-amber-50 border-2 placeholder:text-gray-400 placeholder:pl-5 pt-2 placeholder:vertical-center pl-3"
                 />
                 <label className="text-white pb-3">Email</label>
                 <input
                   type="email"
                   placeholder="Enter Email"
+                  name="email"
                   className="rounded-2xl pb-3 mb-3 text-white border-amber-50 border-2 placeholder:text-gray-400 placeholder:pl-5 pt-2 placeholder:vertical-center pl-3"
                 />
               </div>
@@ -27,12 +53,14 @@ const Message = () => {
                 <input
                   type="tel"
                   placeholder="Enter Phone Number"
+                  name="phone"
                   className="rounded-2xl pb-3 mb-3 text-white border-amber-50 border-2 placeholder:text-gray-400 placeholder:pl-5 pt-2 placeholder:vertical-center pl-3"
                 />
                 <label className="text-white pb-3">Message</label>
                 <textarea
                   rows="1"
                   placeholder="Enter your message"
+                  name="message"
                   className="rounded-2xl pb-3 mb-3 text-white border-amber-50 border-2 placeholder:text-gray-400 placeholder:pl-5 pt-2 placeholder:vertical-center pl-3"
                 />
               </div>
@@ -45,6 +73,7 @@ const Message = () => {
                 Send Message
               </button>
             </div>
+            <span className="text-white text-center mx-auto">{result}</span>
           </form>
         </div>
       </div>
