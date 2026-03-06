731c4be585e06dccfccef79127b5b797bc06488d
Abhinav Verma <abhinavverma658@gmail.com>
2026-03-07 00:16:01 +0530

    adding new files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import Navbar from "./components/navbar.jsx";
+import HeroSection from "./components/herosection.jsx";
+import Stats from "./components/stats.jsx";
+import AboutUs from "./components/about.jsx";
+import TechnicalSkills from "./components/technicalskills.jsx";
+import Project from "./components/featured.jsx";
+import Experience from "./components/experience.jsx";
+import Message from "./components/leaveMessage.jsx";
+import Contact from "./components/contact.jsx";
+import Cursor from "./components/cursor.jsx";
+import BackToTopButton from "./components/backToTop.jsx";
+function App() {
+  return (
+    <ThemeProvider>
+      <Cursor />
+      <Navbar />
+      <div className=" bg-[#f8f8f5] dark:bg-black">
+        <HeroSection />
+        <Stats />
+        <AboutUs />
+        <TechnicalSkills />
+        <Project />
+        <Experience />
+        <Message />
+        <Contact />
+      </div>
+
+      <BackToTopButton />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/skill.jsx
@@ -0,0 +1,8 @@
+const Skill = (props) => {
+  return (
+    <div className="rounded-full dark:bg-[#121212] p-3 max-w-full text-center text-sm font-medium dark:text-white border border-white dark:border-gray-800 hover:bg-gray-700 hover:text-white transition-colors duration-300">
+      {props.skill}
+    </div>
+  );
+};
+export default Skill;
